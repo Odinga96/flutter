316a05b6b2ddbbf32da93a87b4cbf9e9ad8da433
odinga96 <divadagnido96@gmail.com>
2023-08-24 03:14:19 +0300

    Update main page

--- track_expense/lib/main.dart
+++ track_expense/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:track_expense/screens/expense_report.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: const  Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: const ExpenseReport(title: 'Expenses'),
       theme: ThemeData(primarySwatch: Colors.orange),
     );
   }
